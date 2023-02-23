bc7e9ae214c385f3f450fd52fd88e772b793d1a8
Ehsanur Rahman Rhythm <ehsanur.rahman.rhythm@g.bracu.ac.bd>
2023-02-23 19:10:58 +0600

    userFactory fix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,8 @@ class UserFactory extends Factory
         $email = str_replace(' ', '.', $name) . '@' . ($role == 'student' ? 'g.' : '') . 'bracu.ac.bd';
         // remove if there is 2 dots
         $email = str_replace('..', '.', $email);
+        // the email should be lowercase
+        $email = strtolower($email);
         $safe_name = preg_replace('/[^A-Za-z0-9\-]/', '', $name);
         // $university_id = $role == 'student' ? $this->faker->randomNumber(8) : $initials;
         // generate an id starts with "20 or 21 or 22 or 23" and then 6 random numbers
