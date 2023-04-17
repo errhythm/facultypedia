9fd2af0da1a94795d67b6607107bd80c8d38295d
Ehsanur Rahman Rhythm <ehsanur.rahman.rhythm@g.bracu.ac.bd>
2023-04-18 01:38:32 +0600

    Added schemas for consultation, tygpt4

---
+++ database/migrations/2023_04_17_193223_create_consultation_slots_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('consultation_slots', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('faculty_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->enum('day_of_week', ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('consultation_slots');
+    }
+};
---
+++ database/migrations/2023_04_17_193228_create_consultations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('consultations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('slot_id')->constrained('consultation_slots')->onDelete('cascade')->onUpdate('cascade');
+            $table->date('consultation_date');
+            $table->string('meeting_link');
+            $table->enum('is_approved', ['Pending', 'Approved', 'Rejected'])->default('Pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('consultations');
+    }
+};
